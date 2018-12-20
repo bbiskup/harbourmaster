3a190d9ac705219b277c2f4206d7738c4214b731
Bernhard Biskup <bernhard@fract-tech.com>
2018-12-21 00:31:56 +0100

    Server index.html and elm.js from Flask app

--- python/harbourmaster.py
+++ python/harbourmaster.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restplus import Resource, Api
 import requests
 import requests_unixsocket
 
 
 app = Flask(__name__)
-api = Api(app)
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+
+api = Api(blueprint, doc='/doc/')
+app.register_blueprint(blueprint)
 
 session = requests_unixsocket.Session()
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    print('##### HIER')
+    return app.send_static_file('index.html')
 
 docker_engine_parser = api.parser()
 docker_engine_parser.add_argument('url',
