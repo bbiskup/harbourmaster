5ff92b89d942d080b69557b16437ee471f1f5bac
Bernhard Biskup <bernhard@fract-tech.com>
2018-12-28 11:28:45 +0100

    Parse & display container run state in container list

--- elm/src/Pages/Containers.elm
+++ elm/src/Pages/Containers.elm
@@ -1,5 +1,9 @@
 module Pages.Containers exposing (Model, Msg, init, subscriptions, update, view)
 
+{-| Container list view
+-}
+
+import Bootstrap.Form.Checkbox as Checkbox
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
 import Bootstrap.Table as Table
@@ -11,14 +15,26 @@ import Json.Decode.Pipeline exposing (..)
 import Routes exposing (containerPath)
 
 
+{-| Run state of a Docker container
+See <https://docs.docker.com/engine/api/v1.39/#tag/Container>
+-}
+type RunState
+    = Created
+    | Restarting
+    | Running
+    | Removing
+    | Paused
+    | Exited
+    | Dead
 
-{- Short information on Docker container, as returned by /containers/json endpoint -}
 
-
+{-| Short information on Docker container, as returned by /containers/json endpoint
+-}
 type alias DockerContainer =
     { id : String
     , names : List String
     , image : String
+    , state : RunState
     , status : String
     , command : String
     }
@@ -34,6 +50,7 @@ dockerContainerDecoder =
         |> required "Id" Decode.string
         |> required "Names" (Decode.list Decode.string)
         |> required "Image" Decode.string
+        |> required "State" runStateDecoder
         |> required "Status" Decode.string
         |> required "Command" Decode.string
 
@@ -42,6 +59,63 @@ dockerContainersDecoder : Decode.Decoder DockerContainers
 dockerContainersDecoder =
     Decode.map DockerContainers <|
         Decode.list dockerContainerDecoder
+
+
+runStateDecoder : Decode.Decoder RunState
+runStateDecoder =
+    Decode.string
+        |> Decode.andThen
+            (\str ->
+                case str of
+                    "created" ->
+                        Decode.succeed Created
+
+                    "restarting" ->
+                        Decode.succeed Restarting
+
+                    "running" ->
+                        Decode.succeed Running
+
+                    "removing" ->
+                        Decode.succeed Removing
+
+                    "paused" ->
+                        Decode.succeed Paused
+
+                    "exited" ->
+                        Decode.succeed Exited
+
+                    "dead" ->
+                        Decode.succeed Dead
+
+                    other ->
+                        Decode.fail <| "Unknown run state: " ++ other
+            )
+
+
+showRunState : RunState -> String
+showRunState runState =
+    case runState of
+        Created ->
+            "created"
+
+        Restarting ->
+            "restarting"
+
+        Running ->
+            "running"
+
+        Removing ->
+            "removing"
+
+        Paused ->
+            "paused"
+
+        Exited ->
+            "exited"
+
+        Dead ->
+            "dead"
 
 
 getDockerContainers : Cmd Msg
@@ -98,6 +172,7 @@ viewContainers containers =
             Table.simpleThead
                 [ Table.th [] [ text "Name" ]
                 , Table.th [] [ text "Image" ]
+                , Table.th [] [ text "State" ]
                 , Table.th [] [ text "Status" ]
                 , Table.th [] [ text "Command" ]
                 ]
@@ -117,6 +192,8 @@ viewContainerRow container =
     Table.tr []
         [ Table.td [] [ a [ href <| containerPath container.id ] [ containerName ] ]
         , Table.td [] [ text container.image ]
+        , Table.td []
+            [ text <| showRunState container.state ]
         , Table.td [] [ text container.status ]
         , Table.td [] [ code [] [ text container.command ] ]
         ]
