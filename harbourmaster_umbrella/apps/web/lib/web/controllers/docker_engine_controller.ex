a29c70121d33fee94afc83da4fb70d64359c09e2
Bernhard Biskup <bernhard@fract-tech.com>
2019-02-17 19:39:30 +0100

    Encode query parameters correctly when forwarding request to Docker

--- harbourmaster_umbrella/apps/web/lib/web/controllers/docker_engine_controller.ex
+++ harbourmaster_umbrella/apps/web/lib/web/controllers/docker_engine_controller.ex
@@ -1,11 +1,29 @@
 defmodule Web.DockerEngineController do
   use Web, :controller
 
-  def index(conn, _params) do
-    :logger.info(IO.inspect(_params))
+  def index(conn, params) do
+    :logger.info(IO.inspect(params))
+
+    docker_api_url_prefix = "http://localhost:8000"
+
+    [path, query] =
+      case String.split(params["url"], "?", parts: 2) do
+        [url, query] ->
+          IO.puts("URL+query #{url} #{query}")
+          [url, URI.encode(query)]
+
+        [url] ->
+          IO.puts("Only URL #{url}")
+          [url, '']
+      end
+
+    docker_api_url =
+      String.to_char_list("#{docker_api_url_prefix}#{path}?") ++ :http_uri.encode(query)
+
+    # :logger.debug("docker_api_url #{IO.inspect(docker_api_url)}")
 
     {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} =
-      :httpc.request(:get, {'http://localhost:8000/images/json', []}, [], [])
+      :httpc.request(:get, {docker_api_url, []}, [], [])
 
     conn
     |> put_resp_content_type("application/json")
